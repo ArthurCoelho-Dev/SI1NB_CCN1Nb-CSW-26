3d64db45e6024a55842dbfa3db9b198a5bf9ef9e
ArthurCoelho-Dev <arthurcoelho056@gmail.com>
2026-05-13 22:37:40 -0300

    Add files via upload

---
+++ Aula 14/javascript.js
@@ -0,0 +1,19 @@
+var x = 30
+if (x > 5)
+    (console.log("x is greater than S")
+)
+ else if (x < 5)
+    (console.log("x is less than S")
+)
+else (console.log("x is equal than S")
+)
+
+for (var 1 = 0; 1 < 10; 1++)
+(if (1 % 2 === 0)
+(console.log(1): 0, 2, 4, 6, 8);
+)
+
+for (let 1 = 0; 1 < 10; 1 += 2)
+(console.log(1); 0, 2, 4, 6, 8
+
+);
