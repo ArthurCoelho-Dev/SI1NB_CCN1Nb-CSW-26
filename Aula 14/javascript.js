31f022d18ea7181071bfc97c4f9901414f54d659
ArthurCoelho-Dev <arthurcoelho056@gmail.com>
2026-05-19 22:30:19 -0300

    Add files via upload

--- Aula 14/javascript.js
+++ Aula 14/javascript.js
@@ -1,19 +1,40 @@
-var x = 30
-if (x > 5)
-    (console.log("x is greater than S")
-)
- else if (x < 5)
-    (console.log("x is less than S")
-)
-else (console.log("x is equal than S")
-)
+var IssoEumaString = 'Isso é uma String';
+let IssoEumNumero = 42;
+const IssoEumBooleano = true;
+let IssoTambemEumaString = '42';
 
-for (var 1 = 0; 1 < 10; 1++)
-(if (1 % 2 === 0)
-(console.log(1): 0, 2, 4, 6, 8);
-)
+if (IssoEumNumero === IssoTambemEumaString) {
+    console.log("As String são Iguais");
+} else {
+    console.log("As Strings são Diferentes");
+}
 
-for (let 1 = 0; 1 < 10; 1 += 2)
-(console.log(1); 0, 2, 4, 6, 8
+console.log(IssoEumNumero + IssoTambemEumaString);
 
-);
+console.log(IssoEumNumero + IssoEumNumero)
+
+var IssoEumaGlobal = "Eu sou uma variável Global";
+{
+let IssoEumaLocal = "Eu sou uma variávek Local"
+console.log(IssoEumaGlobal)
+console.log(IssoEumaLocal)
+}
+
+console.log(IssoEumaGlobal);
+
+let floatvalue1 = 0.06
+let floatvalue2 = 0.01
+let sum = floatvalue1 + floatvalue2;
+
+console.log(sum. toPrecision(2))
+
+for (let numeros = 0; numeros < 5; numeros ++) {
+  console.log(numeros);
+  console.log(numeros*2);
+}
+
+let j = 0;
+while (j < 5) {
+console.log(j*2+1);
+j++;
+}
